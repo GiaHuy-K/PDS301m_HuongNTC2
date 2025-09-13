ac7706d3e68ce0f3439cdf97e411df771e035967
Gia Huy <chutran01012004@gmail.com>
2025-09-13 21:00:58 +0700

    done 1.5 Logical Operators & Boolean Variables

---
+++ Basic/Exercise1/1.5.Logical Operators & Boolean Variables/Basic1.5.py
@@ -0,0 +1,21 @@
+x = 5
+y = 2
+print(x > y)
+print(not (x > y))
+print(x == y)
+
+print(True & False)
+
+print(2*(x>y))
+j = (x>y)+(x==y)+(y>x)
+print(j)
+
+print((x>y)|(y>x))
+print((x>y)&(y>x))
+print((x>y)|(x==y))
+print(not (x==y))
+print((x>y)|(-1*(x==y))==True)
+# ➦ This directs you to do something specific, maybe in the operating system or answer something conceptual.
+# ⌨ Coding you need to do, in the subsequent code cell.
+# ❔ Questions to answer in the same markdown cell.
+# ⛬ Similar to a question, but requesting an interpretation you need to provide, in the same markdown cell
